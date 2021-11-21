4d5f1affa10f3bb2b76b33b571f31566b675b4f4
Salisu Shuaibu <salisshuaibu11@gmail.com>
2021-11-21 09:56:26 +0100

    Deleted birthdayLine

--- birthdayLineV1-main/components/BackButton.tsx
+++
@@ -1,9 +0,0 @@
-import { ArrowLeftIcon } from "@heroicons/react/outline";
-import { useRouter } from "next/router";
-
-const GoBackToPreviousPage = () => {
-    const router = useRouter();
-    return <ArrowLeftIcon onClick={() => router.back()} width={30} style={{color: "#4653EF"}} />
-}
-
-export default GoBackToPreviousPage;
